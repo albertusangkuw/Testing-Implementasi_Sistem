5feec38e7b04ce1f5556f788b06d43ad5f63dfbc
Albertus A <angkuwjr@yahoo.com>
2021-04-30 20:03:31 +0700

    Complete login with google

--- Project/app/src/main/java/com/tubes/emusic/api/SessionApi.kt
+++ Project/app/src/main/java/com/tubes/emusic/api/SessionApi.kt
@@ -1,9 +1,14 @@
 package com.tubes.emusic.api
 
 import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
+import com.tubes.emusic.entity.User
 import cz.msebera.android.httpclient.Header
 import kotlinx.coroutines.delay
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SessionApi {
 
@@ -123,6 +128,60 @@ class SessionApi {
 
             delay(300L)
             return cookieStatus
+        }
+
+        public suspend fun signwithGoogle(newUser: GoogleSignInAccount) : Boolean{
+            //Try to login first if user already have account
+            if(loginUser(newUser.email!!, newUser.id!!)){
+                return true
+            }
+            delay(500L)
+
+            var url =  HTTPClientManager.host + "registrasi"
+            val params = RequestParams()
+            params.put("username", newUser.givenName)
+            params.put("email", newUser.email)
+            params.put("password", newUser.id)
+            params.put("country", "Indonesia")
+            params.put("dateJoin",  SimpleDateFormat("yyyy-MM-dd").format(Date()).toString())
+            params.put("categories", "2")
+            params.put("urlphotoprofile", "")
+
+            HTTPClientManager.client.post(url, params, object : AsyncHttpResponseHandler() {
+                override fun onSuccess(statusCode: Int, headers: Array<Header>,
+                                       responseBody: ByteArray) {
+                    val result = String(responseBody)
+                    Log.d("API", "Hasil ${result}")
+                    if (HTTPClientManager.getStatusRequest(result)) {
+                        Log.d("API", "Success Insert User")
+                        status = true
+                    } else {
+                        Log.d("API", "Failed Insert User")
+                        status = false
+                    }
+                }
+
+                override fun onFailure(statusCode: Int, headers: Array<Header>, responseBody: ByteArray, error: Throwable) {
+                    // Jika koneksi gagal
+                    status = false
+                    val errorMessage = when (statusCode) {
+                        401 -> "$statusCode : Bad Request"
+                        403 -> "$statusCode : Forbidden"
+                        404 -> "$statusCode : Not Found"
+                        else -> "$statusCode : ${error.message}"
+                    }
+                    Log.d("Failure", errorMessage)
+                }
+
+                // ----New Overridden method
+                override fun getUseSynchronousMode(): Boolean {
+                    return false
+                }
+
+            })
+
+            delay(500L)
+            return status
         }
 
     }
